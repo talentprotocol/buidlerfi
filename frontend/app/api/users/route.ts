28ec2432f93e6a9e649779d3d1515db457e342a4
Ruben Dinis <ruben.sousa.dinis@gmail.com>
2023-10-04 14:18:49 +0100

    Apply new prettier config

--- frontend/app/api/users/route.ts
+++ frontend/app/api/users/route.ts
@@ -1,16 +1,18 @@
-import UpsertUser from '@/backend/services/users/upsert';
+import UpsertUser from "@/backend/services/users/upsert";
 
 export async function PUT(req: Request) {
-	try {
-		const body = await req.json();
-		if (!body.wallet) {
-			return Response.json({ error: 'Wallet field is mandatory' }, { status: 409 });
-		}
-		const user = await UpsertUser.call(body.wallet);
+  try {
+    const body = await req.json();
+    if (!body.wallet) {
+      return Response.json({ error: "Wallet field is mandatory" }, { status: 409 });
+    }
+    const user = await UpsertUser.call(body.wallet);
 
-		return Response.json(user, { status: 200 });
-	} catch (error) {
-		console.error(error);
-		return Response.json({ error: 'Unexpected error. Contact Us.' }, { status: 500 });
-	}
+    return Response.json(user, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    console.error("Error url:", req.url);
+    console.error("Error body:", await req.json());
+    return Response.json({ error: "Unexpected error. Contact Us." }, { status: 500 });
+  }
 }
