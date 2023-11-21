3170c2ea17f3d0235778c81ea0ce8dbcf7c50e96
francisco-leal <francisco@talentprotocol.com>
2023-11-21 19:46:54 +0000

    Add create wallet screen

--- frontend/app/(anonymous)/onboarding/welcome/page.tsx
+++ frontend/app/(anonymous)/onboarding/welcome/page.tsx
@@ -10,16 +10,22 @@ export default function WelcomePage() {
 
   const navigateToFund = () => {
     setButtonLoading(true);
-    router.push("/onboarding/fund");
+    setTimeout(() => router.push("/onboarding/fund"), 500);
   };
 
   return (
     <Flex y grow gap2 component={"main"} px={4} py={2}>
-      <Typography level="h3">Generating your builder.fi wallet</Typography>
+      <Typography level="h3">Your builder.fi wallet</Typography>
       <Flex y mb={4} mt={2} gap2>
         <Typography level="body-md" mb={12}>
-          You'll have your own wallet address within builder.fi. You'll be able to deposit and withdraw ETH in Base and
-          to make the experience of trading keys very smooth!
+          welcome!
+          <br />
+          builder.fi will automatically create a new self-custodial wallet for you, that you fully own and control.
+          <br />
+          the onboarding can feel complex, but the experience will be super smooth after you&apos;re in. builder
+          promise.
+          <br />
+          thanks for being an early supporter and helping us test the app.
         </Typography>
         <Button loading={buttonLoading} onClick={() => navigateToFund()}>
           Create wallet
