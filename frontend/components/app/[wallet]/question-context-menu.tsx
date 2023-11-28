d4cc2f475b69f0e3df144f118cec6937b544b604
0xForkHunter <0xforkhunter@protonmail.com>
2023-11-28 13:49:43 +0700

    hide edit and delete if not owner of question

--- frontend/components/app/[wallet]/question-context-menu.tsx
+++ frontend/components/app/[wallet]/question-context-menu.tsx
@@ -1,31 +1,46 @@
+import { useProfileContext } from "@/contexts/profileContext";
+import { useUserContext } from "@/contexts/userContext";
+import { useGetQuestions } from "@/hooks/useQuestionsApi";
 import { DeleteOutline, EditOutlined, FileUploadOutlined, MoreHoriz } from "@mui/icons-material";
 import { Dropdown, ListItemDecorator, Menu, MenuButton, MenuItem } from "@mui/joy";
+import { FC } from "react";
 
-export const QuestionContextMenu = () => {
+interface Props {
+  question?: NonNullable<ReturnType<typeof useGetQuestions>["data"]>[number];
+}
+
+export const QuestionContextMenu: FC<Props> = ({ question }) => {
+  const {} = useProfileContext();
+  const { user } = useUserContext();
+  const isQuestionAsker = question?.questionerId === user?.id;
   return (
     <Dropdown>
       <MenuButton variant="plain">
         <MoreHoriz />
       </MenuButton>
       <Menu>
-        <MenuItem>
-          <ListItemDecorator>
-            <EditOutlined />
-          </ListItemDecorator>
-          Edit
-        </MenuItem>
+        {isQuestionAsker && (
+          <MenuItem>
+            <ListItemDecorator>
+              <EditOutlined />
+            </ListItemDecorator>
+            Edit
+          </MenuItem>
+        )}
         <MenuItem>
           <ListItemDecorator>
             <FileUploadOutlined />
           </ListItemDecorator>
           Share
         </MenuItem>
-        <MenuItem color="danger">
-          <ListItemDecorator>
-            <DeleteOutline />
-          </ListItemDecorator>
-          Delete
-        </MenuItem>
+        {isQuestionAsker && (
+          <MenuItem color="danger">
+            <ListItemDecorator>
+              <DeleteOutline />
+            </ListItemDecorator>
+            Delete
+          </MenuItem>
+        )}
       </Menu>
     </Dropdown>
   );
