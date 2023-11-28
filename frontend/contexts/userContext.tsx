46402249dc09bef39fc6617790bb5f700399fe11
0xForkHunter <0xforkhunter@protonmail.com>
2023-11-28 21:19:16 +0700

    revert isLoading condition

--- frontend/contexts/userContext.tsx
+++ frontend/contexts/userContext.tsx
@@ -64,7 +64,7 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
     () => ({
       user: user.data,
       privyUser: privyUser || undefined,
-      isLoading: !ready || user.isLoading,
+      isLoading: !ready || (!!privyUser && user.isLoading),
       isAuthenticatedAndActive: ready && !user.isLoading && !!user.data && user.data.isActive && privyAuthenticated,
       address: user.data?.wallet ? (user.data?.wallet as `0x${string}`) : undefined,
       socialAddress: socialWallet as `0x${string}`,
