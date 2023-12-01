62b79757c69777fbd2a29c1206f60936d8d58730
lim0n3 <simonestaffa96@gmail.com>
2023-12-01 14:09:26 +0100

    Farcaster api functions

---
+++ frontend/lib/api/backend/farcaster.ts
@@ -0,0 +1,58 @@
+import { NeynarAPIClient } from "@standard-crypto/farcaster-js-neynar";
+
+export const publishCast = async (text: string) => {
+  if (!process.env.FARCASTER_API_KEY || !process.env.FARCASTER_SIGNER_UUID) {
+    throw new Error("FARCASTER_API_KEY and FARCASTER_SIGNER_UUID must be set in the environment");
+  }
+  const signerUuid = process.env.FARCASTER_SIGNER_UUID as string;
+  const client = new NeynarAPIClient(process.env.FARCASTER_API_KEY as string);
+
+  const publishedCast = await client.v2.publishCast(signerUuid, text);
+
+  console.log(`New cast hash: ${publishedCast.hash}`);
+
+  return publishedCast.hash;
+};
+
+export const replyToCast = async (existingCastHash: string, reply: string) => {
+  if (!process.env.FARCASTER_API_KEY || !process.env.FARCASTER_SIGNER_UUID) {
+    throw new Error("FARCASTER_API_KEY and FARCASTER_SIGNER_UUID must be set in the environment");
+  }
+  const signerUuid = process.env.FARCASTER_SIGNER_UUID as string;
+  const client = new NeynarAPIClient(process.env.FARCASTER_API_KEY as string);
+
+  const publishedCast = await client.v2.publishCast(signerUuid, reply, { replyTo: existingCastHash });
+
+  console.log(`Reply hash:${publishedCast.hash}`);
+
+  return publishedCast.hash;
+};
+
+const NEW_BUILDERFI_QUESTION_CAST =
+  "{questionAuthor} just asked {questionRecipient} a question on builder.fi!\n\n{link}";
+const NEW_BUILDERFI_QUESTION_PARENT_CAST_HASH = "0x";
+
+const NEW_BUILDERFI_ANSWER_CAST = "{replyAuthor} just answered {questionAuthor} question on builder.fi!\n\n{link}";
+const NEW_BUILDERFI_ANSWER_PARENT_CAST_HASH = "0x";
+
+const NEW_BUILDERFI_USER_CAST = "{user} just launched their keys on builder.fi!\n\n{link}";
+const NEW_BUILDERFI_USER_PARENT_CAST_HASH = "0x";
+
+export const publishNewQuestionCast = async (questionAuthor: string, questionRecipient: string, link: string) => {
+  const text = NEW_BUILDERFI_QUESTION_CAST.replace("{questionAuthor}", questionAuthor)
+    .replace("{questionRecipient}", questionRecipient)
+    .replace("{link}", link);
+  return replyToCast(NEW_BUILDERFI_QUESTION_PARENT_CAST_HASH, text);
+};
+
+export const publishNewAnswerCast = async (replyAuthor: string, questionAuthor: string, link: string) => {
+  const text = NEW_BUILDERFI_ANSWER_CAST.replace("{replyAuthor}", replyAuthor)
+    .replace("{questionAuthor}", questionAuthor)
+    .replace("{link}", link);
+  return replyToCast(NEW_BUILDERFI_ANSWER_PARENT_CAST_HASH, text);
+};
+
+export const publishNewUserCast = async (user: string, link: string) => {
+  const text = NEW_BUILDERFI_USER_CAST.replace("{user}", user).replace("{link}", link);
+  return replyToCast(NEW_BUILDERFI_USER_PARENT_CAST_HASH, text);
+};
