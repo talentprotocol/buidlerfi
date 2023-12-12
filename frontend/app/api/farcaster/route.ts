0e2246407fc7563c24913f7173c56d72013dfbee
lim0n3 <simonestaffa96@gmail.com>
2023-12-12 17:45:58 +0100

    add more interfaces

--- frontend/app/api/farcaster/route.ts
+++ frontend/app/api/farcaster/route.ts
@@ -1,6 +1,7 @@
 import { BUILDERFI_FARCASTER_FID } from "@/lib/constants";
 import { NeynarAPIClient } from "@neynar/nodejs-sdk";
-import { CastWithInteractions } from "@neynar/nodejs-sdk/build/neynar-api/v2";
+import { CastWithInteractions as CastWithInteractionsV1 } from "@neynar/nodejs-sdk/build/neynar-api/v1";
+import { CastWithInteractions as CastWithInteractionsV2 } from "@neynar/nodejs-sdk/build/neynar-api/v2";
 
 export const GET = async () => {
   const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY as string);
@@ -19,22 +20,34 @@ export const GET = async () => {
         // notifications that have only one mention (the recipient of the question)
         n.mentionedProfiles?.filter(p => p.fid !== BUILDERFI_FARCASTER_FID)?.length === 1
     )
-    .map(n => parseMentionNotification(n as any));
+    .map(n => parseMentionNotification(n as unknown as CastWithInteractionsV2));
 
   const questions = await getQuestionsFromNotifications(mentionNotifications);
-  const mostRecentTimestamp = mentionNotifications[0].timestamp;
+
   // TODO: create questions on DB
+  
+  const mostRecentTimestamp = mentionNotifications[0].timestamp;
   // TODO: store mostRecentTimestamp on DB
 };
 
-const getQuestionsFromNotifications = async (notifications: any[]) => {
+export interface ParsedMentionNotification {
+  authorUsername: string;
+  authorFid: number;
+  mentionedProfiles: { username: string; fid: number }[];
+  timestamp: string;
+  text: string;
+}
+
+const getQuestionsFromNotifications = async (
+  notifications: ParsedMentionNotification[]
+): { recipientUsername: string; questionContent: string }[] => {
   // TODO: integrate with OpenAI here
 };
 
-const parseMentionNotification = (notification: CastWithInteractions) => ({
+const parseMentionNotification = (notification: CastWithInteractionsV2) => ({
   authorUsername: notification.author.username,
   authorFid: notification.author.fid,
-  mentionedProfiles: (notification as any).mentionedProfiles?.map(profile => ({
+  mentionedProfiles: (notification as unknown as CastWithInteractionsV1).mentionedProfiles?.map(profile => ({
     username: profile.username,
     fid: profile.fid
   })),
