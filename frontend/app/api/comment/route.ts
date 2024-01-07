8f7ad507a0582e3e42d1e269af43126fedfd533e
0xForkHunter <0xforkhunter@protonmail.com>
2024-01-07 12:47:51 +0700

    attempt to fix DYNAMIC_SERVER_USAGE build errors

--- frontend/app/api/comment/route.ts
+++ frontend/app/api/comment/route.ts
@@ -3,14 +3,14 @@ import { ERRORS } from "@/lib/errors";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const questionId = req.nextUrl.searchParams.has("questionId")
-    ? Number(req.nextUrl.searchParams.get("questionId"))
-    : undefined;
-  const privyUserId = req.headers.get("privyUserId");
-  if (!privyUserId || !questionId) return NextResponse.json({ error: ERRORS.INVALID_REQUEST }, { status: 400 });
-
   try {
-    return NextResponse.json(await getComments(privyUserId, questionId));
+    const questionId = req.nextUrl.searchParams.has("questionId")
+      ? Number(req.nextUrl.searchParams.get("questionId"))
+      : undefined;
+    const privyUserId = req.headers.get("privyUserId");
+    if (!privyUserId || !questionId) return NextResponse.json({ error: ERRORS.INVALID_REQUEST }, { status: 400 });
+    const res = await getComments(privyUserId, questionId);
+    return NextResponse.json(res);
   } catch (err) {
     console.error(err);
     return NextResponse.json({ error: ERRORS.SOMETHING_WENT_WRONG }, { status: 500 });
