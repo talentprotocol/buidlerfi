f6e71d277158051020f2555fd55d2bf873485e83
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2024-01-25 18:20:10 +0100

    removed builderFI user id filter for cast authors in ama

--- frontend/app/api/point/farcaster/route.ts
+++ frontend/app/api/point/farcaster/route.ts
@@ -2,8 +2,10 @@ import { BUILDERFI_FARCASTER_FID } from "@/lib/constants";
 import { ERRORS } from "@/lib/errors";
 import prisma from "@/lib/prisma";
 import { NeynarAPIClient } from "@neynar/nodejs-sdk";
-import { SocialProfileType } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+
+/// This endpoint is used to fetch all casts of an AMA and save them to the database in order to assign points to the users who asked the questions.
+/// We don't check if the Farcaster user is a builderFI user because we want to assing points to users who are not registered on builderFI yet but could be in the future.
 
 export async function GET(req: NextRequest) {
   try {
@@ -25,47 +27,23 @@ export async function GET(req: NextRequest) {
     const farcasterProfiles = await client.fetchBulkUsers(authorsFids, { viewerFid: BUILDERFI_FARCASTER_FID });
     const fidToUsernameMap = new Map(farcasterProfiles.users.map(user => [user.fid, user.username]));
 
-    // Find Builderfi accounts by Farcaster profileName and map FID to UserID
-    const authorsWithBuilderfiAccount = await prisma.socialProfile.findMany({
-      where: {
-        type: SocialProfileType.FARCASTER,
-        profileName: { in: Array.from(fidToUsernameMap.values()) }
-      },
-      include: {
-        user: true
-      }
-    });
-
-    // Create a map from profileName to UserId
-    const profileNameToUserIdMap = new Map(
-      authorsWithBuilderfiAccount.map(profile => [profile.profileName, profile.userId])
-    );
-
-    // Filter casts by authors with a Builderfi account
-    const builderfiAuthorsCasts = casts.filter(cast => {
-      const username = fidToUsernameMap.get(Number(cast.author.fid));
-      return username && profileNameToUserIdMap.has(username);
-    });
-
     // Prepare the data for saving to the database
-    const castInteractionData = builderfiAuthorsCasts.map(cast => {
+    const castInteractionData = casts.map(cast => {
       const authorUsername = fidToUsernameMap.get(Number(cast.author.fid)) || 'default';
-      const userId = profileNameToUserIdMap.get(authorUsername!) || 0;
 
       return {
-        parentCastHash: castHash, // The original cast hash from the request
-        replyCastHash: cast.hash, 
-        replyCastText: cast.text,
-        authorFid: Number(cast.author.fid),
-        authorName: authorUsername,
-        userId: userId
+        parentCastHash: castHash, // The parent cast hash from the request
+        replyCastHash: cast.hash, // The hash of the reply cast
+        replyCastText: cast.text, // The text of the reply cast
+        authorFid: Number(cast.author.fid), // The fid of the reply cast author
+        authorName: authorUsername // The Farcaster username of the reply cast author
       };
     });
 
     // Save the cast interactions to the database
     const savedInteractions = await prisma.castInteraction.createMany({
       data: castInteractionData,
-      skipDuplicates: true // To avoid inserting duplicates
+      skipDuplicates: true // Skip duplicates if the cast interaction already exists
     });
 
     // Return the saved interactions in the response
