32e9f9ffe09f143ad3aeb2b76d1ba23d6c161a13
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2024-01-25 14:34:25 +0100

    wip fetch ama casts and users

--- frontend/app/api/point/farcaster/route.ts
+++ frontend/app/api/point/farcaster/route.ts
@@ -1,4 +1,3 @@
-import { getPointsHistory } from "@/backend/point/point";
 import { ERRORS } from "@/lib/errors";
 import { NeynarAPIClient } from "@neynar/nodejs-sdk";
 import { NextRequest, NextResponse } from "next/server";
@@ -17,27 +16,38 @@ export async function GET(req: NextRequest) {
 
     const { result } = await client.fetchAllCastsInThread(castHash);
 
-    // map all the casts to their respective authors fid
-    const authorsFids = result?.casts?.map(cast => cast.author.fid);
+    // retrieve the casts from the result
+    const casts = result?.casts ?? [];
 
-    // create a Set from authorsFids to remove duplicates and convert it back to an array
-    const uniqueAuthorsFids = Array.from(new Set(authorsFids));
+    // Fetch Farcaster profiles
+    const authorsFids = [...new Set(casts.map(cast => Number(cast.author.fid)))];
+    const farcasterProfiles = await client.fetchBulkUsers(authorsFids, { viewerFid: BUILDERFI_FARCASTER_FID });
+    const fidToUsernameMap = new Map(farcasterProfiles.users.map(user => [user.fid, user.username]));
 
-    // convert the authorsFids array to an array of numbers
-    const authorsFidsNumbers = uniqueAuthorsFids.map(fid => Number(fid));
+    // Find Builderfi accounts by Farcaster profileName and map FID to UserID
+    const authorsWithBuilderfiAccount = await prisma.socialProfile.findMany({
+      where: {
+        type: SocialProfileType.FARCASTER,
+        profileName: { in: Array.from(fidToUsernameMap.values()) }
+      },
+      include: {
+        user: true
+      }
+    });
 
-    // get the Farcaster profile names from their FIDs
-    const authors = (await client.fetchBulkUsers(authorsFidsNumbers, { viewerFid: BUILDERFI_FARCASTER_FID })).users;
+    // Create a map from profileName to UserId
+    const profileNameToUserIdMap = new Map(authorsWithBuilderfiAccount.map(profile => [profile.profileName, profile.userId]));
 
-    // get only the profiles with an account on Builderfi
-    // TODO: this is not working
-    const authorsWithBuilderfiAccount = authors.filter(author => author.username.includes(SocialProfileType.FARCASTER));
+    // Filter casts by authors with a Builderfi account
+    const builderfiAuthorsCasts = casts.filter(cast => {
+      const username = fidToUsernameMap.get(Number(cast.author.fid));
+      return username && profileNameToUserIdMap.has(username);
+    });
+    
+    // save on db ....
+   
 
-    const authorsWithBuilderfiAccountFids = authorsWithBuilderfiAccount.map(author => author.fid);
-
-    // for all the authors, get their casts 
-    const builderfiAuthorsCasts = result.casts.filter(cast => authorsWithBuilderfiAccountFids.includes(Number(cast.author.fid)));
-
+    return NextResponse.json({ /*savedCasts*/ }, { status: 200 });
 
   } catch (err) {
     console.error(err);
