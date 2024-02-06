eda232ce5ac9435f1d1b124d9c973490bef51304
0xForkHunter <0xforkhunter@protonmail.com>
2024-02-06 14:16:34 +0700

    Remove isGated change on DB schema

--- frontend/prisma/migrations/20240201051130_add_gated_status/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Comment" ADD COLUMN     "isGated" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "Question" ADD COLUMN     "isGated" BOOLEAN NOT NULL DEFAULT true;
