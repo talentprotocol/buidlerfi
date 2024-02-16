c99c3f29779f0f986f16f944ccaa61b1415d55ae
lim0n3 <simonestaffa96@gmail.com>
2024-02-16 10:27:09 +0100

    minor fixes

--- frontend/app/api/question/[id]/route.ts
+++ frontend/app/api/question/[id]/route.ts
@@ -1,14 +1,63 @@
-import { getQuestion } from "@/backend/question/question";
+import { SocialProfileType } from ".prisma/client";
+import { sendNotification } from "@/backend/notification/notification";
+import { getFarcasterProfileName, publishNewAnswerCast } from "@/lib/api/backend/farcaster";
+import { MAX_COMMENT_LENGTH } from "@/lib/constants";
 import { ERRORS } from "@/lib/errors";
-import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
-export async function GET(req: Request, { params }: { params: { id: number } }) {
+export async function PUT(req: Request, { params }: { params: { id: number } }) {
   try {
-    const privyUserId = req.headers.get("privyUserId");
-    const res = await getQuestion(Number(params.id), privyUserId || undefined);
-    return NextResponse.json(res);
-  } catch (err) {
-    console.error(err);
-    return NextResponse.json({ error: ERRORS.SOMETHING_WENT_WRONG }, { status: 500 });
+    const body = await req.json();
+    const question = await prisma.question.findUnique({ where: { id: Number(params.id) } });
+    const answerContent = body["answerContent"];
+    if (!question || !answerContent) return Response.json({ error: ERRORS.QUESTION_NOT_FOUND }, { status: 404 });
+
+    if (answerContent.length < 5 || answerContent.length > MAX_COMMENT_LENGTH)
+      return Response.json({ error: ERRORS.INVALID_LENGTH }, { status: 400 });
+
+    const replier = await prisma.user.findUniqueOrThrow({
+      where: { privyUserId: req.headers.get("privyUserId")! },
+      include: { socialProfiles: true }
+    });
+
+    if (question.replierId !== replier?.id) return Response.json({ error: ERRORS.UNAUTHORIZED }, { status: 401 });
+
+    const res = await prisma.$transaction(async tx => {
+      const question = await tx.question.update({
+        where: { id: Number(params.id) },
+        data: {
+          reply: body["answerContent"],
+          repliedOn: new Date()
+        }
+      });
+      await sendNotification(question.questionerId, "REPLIED_YOUR_QUESTION", replier.id, question.id, tx);
+      return question;
+    });
+
+    console.log("Farcaster enabled -> ", process.env.ENABLE_FARCASTER);
+    if (process.env.ENABLE_FARCASTER === "true") {
+      const questioner = await prisma.user.findUnique({
+        where: { id: question.questionerId },
+        include: { socialProfiles: true }
+      });
+      const questionerFarcaster = questioner?.socialProfiles.find(sp => sp.type === SocialProfileType.FARCASTER);
+      const replierFarcaster = replier?.socialProfiles.find(sp => sp.type === SocialProfileType.FARCASTER);
+
+      console.log("FOUND questioner -> ", !!questionerFarcaster);
+      console.log("FOUND replier -> ", !!replierFarcaster);
+
+      if (questionerFarcaster || replierFarcaster) {
+        const replierName = getFarcasterProfileName(replier!, replierFarcaster);
+        const questionerName = getFarcasterProfileName(questioner!, questionerFarcaster);
+        // if one of the two has farcaster, publish the cast
+        console.log("CASTING NEW ANSWER");
+        await publishNewAnswerCast(replierName, questionerName, `https://app.builder.fi/question/${question.id}`);
+      }
+    }
+
+    return Response.json({ data: res }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: ERRORS.SOMETHING_WENT_WRONG }, { status: 500 });
   }
 }
