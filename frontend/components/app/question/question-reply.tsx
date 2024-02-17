3fce20efe39b0404655757cfdf1429ae71e4d1d3
0xForkHunter <0xforkhunter@protonmail.com>
2024-02-17 16:06:47 +0700

    Fix wording about gating answer

--- frontend/components/app/question/question-reply.tsx
+++ frontend/components/app/question/question-reply.tsx
@@ -67,7 +67,7 @@ export const QuestionReply: FC<Props> = ({
           />
           {hasLaunchedKeys && (
             <Checkbox
-              label="Gate this response to your key holders only."
+              label="Gate this answer to be visible to your key holders only."
               size="sm"
               checked={isGateAnswer}
               onChange={e => setIsGateAnswer(e.target.checked)}
